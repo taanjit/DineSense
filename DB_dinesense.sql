bd6aaeb66560907f01c52d1b2ab612f816aad925
ann-kannikal <ann.k@thinkpalm.com>
2025-04-21 11:31:39 +0530

    DB containing tables

---
+++ DB_dinesense.sql
@@ -0,0 +1,18 @@
+create database Dine_sense;
+use Dine_sense;
+
+create table Segmentation(GenderRatio int, AgeGroup int);
+                        
+create table WaitingTime(TotalNum int, MenCount int, WomenCount int);
+
+create table TableUtilization(WaiterEngageTime time, TimeToOrder time, TimeToFood time, TimeToBill time, AvgEngageTime time);
+
+create table Heatmap(AvgSector1 int, AvgSector2 int, AvgSector3 int, AvgSector4 int);
+                        
+create table TemperatureRec(TempSec1 varchar(10), TempSec2 varchar(10), TempSec3 varchar(10),TempSec4 varchar(10));
+
+
+
+								
+                            
+                            
